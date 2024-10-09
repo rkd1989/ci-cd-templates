f97168bb7e741a75ec3c84339ff4115cac76e9c8
Rishabh Kavidayal <34520858+rkd1989@users.noreply.github.com>
2024-10-09 15:36:54 -0500

    setting up github actions

---
+++ src/app.test.js
@@ -0,0 +1,21 @@
+const app = require("./app");
+const supertest = require("supertest");
+const request = supertest(app);
+
+describe("Test endpoint responses", () => {
+  it("gets the test endpoint", async () => {
+    const response = await request.get("/test");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Hello World");
+  });
+});
+// Run the test with the following command:
+// npm test
+// PASS  src/app.test.js
+// Test endpoint responses
+//   √ gets the test endpoint (67 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        1.335 s
